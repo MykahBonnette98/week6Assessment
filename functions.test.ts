4a73d2e9108a73d6a79a8b998903b0e99e7728e1
Mykah Bonnette <mykahwalker@Chances-Air.localdomain>
2022-07-01 16:13:17 -0600

    test fix

--- functions.test.ts
+++ functions.test.ts
@@ -1,7 +1,18 @@
 const {shuffleArray} = require('./utils')
+const arr1 = [1,2,3,4]
+const arr2 = []
+const arr3 = ['testing', 'array', 'shuffle']
 
 describe('shuffleArray should', () => {
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(arr1))).toBe(true)
+        expect(Array.isArray(shuffleArray(arr2))).toBe(true)
+        expect(Array.isArray(shuffleArray(arr3))).toBe(true)
+    })
+    test('same length as input array', () => {
+        expect(shuffleArray(arr1)).toHaveLength(arr1.length)
+        expect(shuffleArray(arr2)).toHaveLength(arr2.length)
+        expect(shuffleArray(arr3)).toHaveLength(arr3.length)
+    })
     
-    expect(shuffleArray()).toBe([]);
-    expect(shuffleArray([])).toEqual(shuffleArray([]))
 })
